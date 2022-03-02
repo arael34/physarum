69b2acc74fdffa1131f0507c58561c5d6e0d186a
rushia <jonasg.13452@gmail.com>
2022-03-01 20:06:03 -0500

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,4 @@
 extern crate piston_window;
-//extern crate graphics;
 extern crate opengl_graphics;
 extern crate rand;
 extern crate image;
@@ -10,36 +9,35 @@ clean up types, specifically in sense function
 spawn enum with angle = rngangle + pi/2
 clean up, try to boost performance
 better sense function(1 call instead of 3)
+agents seem to stick to walls on the right and bottom sides
 */
 
 use piston_window::*;
-//use piston::event_loop::{Events, EventLoop, EventSettings};
-//use piston::input::RenderEvent;
 use opengl_graphics::OpenGL;//, Filter, GlGraphics, TextureSettings};
 use rand::*;
 use rand::distributions::Uniform;
 use std::f64::consts::PI;
-use ::image::{ImageBuffer, Rgba};//RgbaImage
-use ::image::imageops::{blur, brighten};
+use ::image::{ImageBuffer, Rgba};
 
 // window settings
-const WIDTH: f64 = 500.;
-const HEIGHT: f64 = 500.;
+const WIDTH: f64 = 800.;
+const HEIGHT: f64 = 800.;
 
 // sim settings
-const AGENTS: usize = 1_000;
-const SENSOR_OFFSET_ANGLE: f64 = PI / 8.;
-const SENSOR_OFFSET_DST: u8 = 14;
+const AGENTS: usize = 50_000;
+const SENSOR_OFFSET_ANGLE: f64 = PI / 10.;
+const SENSOR_OFFSET_DST: u8 = 20;
 const SENSOR_OFFSET_R: isize = 2;
-const TURN_STRENGTH: f64 = PI / 4.;
-const SPAWN_TYPE: SpawnType = SpawnType::CircleIn;
+const TURN_STRENGTH: f64 = PI / 8.;
+const SPAWN_TYPE: SpawnType = SpawnType::Circle;
+const CIRCLE_ANGLE: f64 = PI; // for circle spawn type
 
+#[allow(dead_code)]
 enum SpawnType {
     Random,
-    CircleIn,
-    _CircleOut,
-    _Waterfall,
-    _Point // maybe
+    Circle,
+    Waterfall,
+    Point // maybe
     //whirlpool
 }
 
@@ -56,26 +54,26 @@ impl Agent {
         let mut new_x = self.x + self.ang.cos();
         let mut new_y = self.y + self.ang.sin();
         let mut rng = thread_rng();
-        if new_x > WIDTH || new_x < 0. {
+        if new_x > WIDTH - 1. || new_x < 0. {
             new_x = if 
                         if new_x < 0. 
                             {0.} 
                         else 
                             {new_x} 
-                    > WIDTH
-                        {WIDTH} 
+                    > WIDTH - 1.
+                        {WIDTH - 1.} 
                     else 
                         {new_x};
             self.ang = rng.gen_range(0., 2. * PI);
         }
-        if new_y > HEIGHT || new_y < 0. {
+        if new_y > HEIGHT - 1. || new_y < 0. {
             new_y = if 
                         if new_y < 0. 
                             {0.} 
                         else 
                             {new_y} 
-                    > HEIGHT
-                        {HEIGHT} 
+                    > HEIGHT - 1.
+                        {HEIGHT - 1.} 
                     else 
                         {new_y};
             self.ang = rng.gen_range(0., 2. * PI);
@@ -90,7 +88,7 @@ impl Agent {
         let rng = thread_rng().gen_range(0., 1.);
         if weight_right < weight_forward && weight_forward > weight_left {}
         else if weight_right == weight_left {
-            //self.ang += (rng - 0.5) * TURN_STRENGTH;
+            self.ang += (rng - 0.5) * 0.2 * TURN_STRENGTH;
         }
         else if weight_right > weight_left {
             self.ang += rng * TURN_STRENGTH;
@@ -106,7 +104,7 @@ impl Agent {
         for x in center_x - SENSOR_OFFSET_R..=center_x + SENSOR_OFFSET_R {
             for y in center_y - SENSOR_OFFSET_R..= center_y + SENSOR_OFFSET_R {
                 if x >= 0 && x < WIDTH as isize && y >= 0 && y < HEIGHT as isize {
-                    sum -= img.get_pixel(x as u32, y as u32)[3] as f64 / 255.;
+                    sum += img.get_pixel(x as u32, y as u32)[2] as f64 / 255.;
                 }
             }
         }
@@ -131,19 +129,42 @@ impl Simulation {
                     agent.ang = rng.sample(uniform) * 2. * PI;
                 }
             }
-            SpawnType::CircleIn => {
+            SpawnType::Circle => {
                 for agent in agents.iter_mut() {
                     let angle = rng.sample(uniform) * 2. * PI;
                     let rad = rng.sample(uniform) * (HEIGHT / 2. - 1.);
                     agent.x = WIDTH / 2. + angle.cos() * rad;
                     agent.y = HEIGHT / 2. + angle.sin() * rad;
-                    agent.ang = angle + 3. * PI / 4.;
+                    agent.ang = angle + CIRCLE_ANGLE;
+                }
+            }
+            SpawnType::Waterfall => {
+                for agent in agents.iter_mut() {
+                    agent.x = rng.sample(uniform) * WIDTH;
+                    agent.y = rng.sample(uniform) * HEIGHT;
+                    agent.ang = PI / 2.;
+                }
+            }
+            SpawnType::Point => {
+                for agent in agents.iter_mut() {
+                    agent.x = WIDTH / 2.;
+                    agent.y = HEIGHT / 2.;
+                    agent.ang = rng.sample(uniform) * 2. * PI;
                 }
             }
             _ => {}
         }
         Simulation{agents}
     }
+}
+
+fn reduce_pixel(value: u8) -> u8 {
+    if value > 1 {
+        return value - 1;
+    } else if value > 0 {
+        return 0;
+    }
+    0
 }
 
 fn main() -> () {
@@ -153,20 +174,24 @@ fn main() -> () {
     let mut window:PistonWindow = WindowSettings::new("Simulation", [WIDTH, HEIGHT])
                                   .graphics_api(opengl).exit_on_esc(true).build().unwrap();
 
-    let mut img = ImageBuffer::new(WIDTH as u32 + 1, HEIGHT as u32 + 1);
-    img.fill(225);
+    let mut img = ImageBuffer::new(WIDTH as u32, HEIGHT as u32);
 
     while let Some(en) = window.next() {
         let texture: G2dTexture = Texture::from_image(&mut window.create_texture_context(), &img, &TextureSettings::new()).unwrap();
         window.draw_2d(&en, |c, g, _d| {
+            
             for agent in sim.agents.iter_mut() {
                 agent.check(&img);
                 agent.update();
-                img.put_pixel(agent.x as u32, agent.y as u32, Rgba([0, 150, 150, 150]));
+                img.put_pixel(agent.x as u32, agent.y as u32, Rgba::<u8>([100, 0, 200, 255]));
             }
+            
             image(&texture, c.transform, g);
-            img = brighten(&img, 7);
-            //img = blur(&img, 0.5);
+            for pixel in img.pixels_mut() {
+                pixel[0] = reduce_pixel(pixel[0]); //for different colors
+                pixel[1] = reduce_pixel(pixel[1]);
+                pixel[2] = reduce_pixel(pixel[2]);
+            }
         });
     }
 }
